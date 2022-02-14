dae35d09c845344f1837bbe1619282ad27f8e8e6
Ricardo Lima <ricardo.lxique@gmail.com>
2022-02-14 14:08:45 -0600

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
     siteMetadata: {
-      title: `Pop It Website`,
+      title: `Pop It`,
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
         "gatsby-plugin-image",
         "gatsby-plugin-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+              name: `blog`,
+              path: `${__dirname}/blog`,
+            }
+        },
     ]
 }
